122fb7e8880752192f26687eb2fe27ddb2643a19
Paolo <gasparro.paolo@gmail.com>
2021-07-06 18:43:41 +0200

    Fixed SQL Injection on file Admin/php/emailteam2.php

--- Admin/php/emailteam2.php
+++ Admin/php/emailteam2.php
@@ -12,20 +12,14 @@ $team = $_POST['team'];
 
 if ($id && $team !== null) {
 
+  $query = "SELECT email_t FROM team WHERE codice = ?"; 
+  $stmt = $conn->prepare($query);
+  $stmt->bind_param('s',$team);
+  $result = $stmt->execute();
 
+  if ($result){
 
- $query = ("SELECT email_t FROM team WHERE codice = '$team'"); 
-
-
-$result = mysql_query($query);	
-
-
-if ($result){
-
-
-
-echo('<a href="mailto: '.$result.'"><center> Clicca qui per mandare un avviso al team. </center></a>');
-
-}
+    echo('<a href="mailto: '.$result.'"><center> Clicca qui per mandare un avviso al team. </center></a>');
+  }
 }
 ?>
