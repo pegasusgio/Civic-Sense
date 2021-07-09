ec3b12e1a942ddd312a4d13e2ca896ea6d4b991e
Paolo <gasparro.paolo@gmail.com>
2021-07-09 11:28:13 +0200

    fixed "SQL Injection" on Team/index.php and Team/php/segnalazione.php

--- Team/index.php
+++ Team/index.php
@@ -1,185 +1,188 @@
-<?php session_start()?>
+<?php session_start() ?>
 <!DOCTYPE html>
 <html lang="en">
 
-  <head>
+<head>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
 
-    <title>SB Admin - Tables</title>
+  <title>SB Admin - Tables</title>
 
-    <!-- Bootstrap core CSS-->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Bootstrap core CSS-->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 
-    <!-- Page level plugin CSS-->
-    <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+  <!-- Page level plugin CSS-->
+  <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
 
-    <!-- Custom styles for this template-->
-    <link href="css/sb-admin.css" rel="stylesheet">
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin.css" rel="stylesheet">
 
-	<!-- grafico -->
-	   <link rel="stylesheet" href="css/graficostyle.css">
-	   
-	   
-  </head>
+  <!-- grafico -->
+  <link rel="stylesheet" href="css/graficostyle.css">
 
-  <body id="page-top">
 
-    <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
+</head>
 
-      <a class="navbar-brand mr-1" href=""> Area riservata</a>
+<body id="page-top">
 
-      
-<!-- INIZIO LOGOUT -->     
+  <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
 
- <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
-    <ul class="navbar-nav ml-auto ml-md-0">
-        <li class="nav-item dropdown no-arrow" >
-           <a class="nav-link dropdown-toggle" href="#" title="Logout"  id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-             <i class="fas fa-user-circle fa-fw"></i>
-           </a>
+    <a class="navbar-brand mr-1" href=""> Area riservata</a>
+
+
+    <!-- INIZIO LOGOUT -->
+
+    <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
+      <ul class="navbar-nav ml-auto ml-md-0">
+        <li class="nav-item dropdown no-arrow">
+          <a class="nav-link dropdown-toggle" href="#" title="Logout" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-user-circle fa-fw"></i>
+          </a>
           <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-            <a class="dropdown-item" href="login.html" data-toggle="modal" data-target="#logoutModal" > Logout </a>
+            <a class="dropdown-item" href="login.html" data-toggle="modal" data-target="#logoutModal"> Logout </a>
           </div>
         </li>
-    </ul>
- </form>
-</nav>
+      </ul>
+    </form>
+  </nav>
 
-    <!-- finestra avviso-->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Sei sicuro di voler lasciare il sito?</h5>
-            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">×</span>
-            </button>
-          </div>
-          <div class="modal-body">Clicca "Logout" per uscire dal sito.</div>
-          <div class="modal-footer">
-            <button class="btn btn-secondary" type="button" data-dismiss="modal">Annulla</button>
-            <a class="btn btn-primary" href="login.php">Logout</a>
-          </div>
+  <!-- finestra avviso-->
+  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Sei sicuro di voler lasciare il sito?</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body">Clicca "Logout" per uscire dal sito.</div>
+        <div class="modal-footer">
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">Annulla</button>
+          <a class="btn btn-primary" href="login.php">Logout</a>
         </div>
       </div>
     </div>
+  </div>
 
-<!-- FINE LOGOUT-->
-
-
-    <div id="wrapper">
-
-          <div class="card mb-3">
-           
-			
-			<!-- MAPPA -->
-	
-  <style>
-    #map{
-      height:500px;
-      width:100%; 
-      margin-left:0%;
-    }
-    *{
-      margin:0;
-      padding:0;
-    }
-  </style>
+  <!-- FINE LOGOUT-->
 
 
-  <div id="map"></div>
-    
-    <script>
-      function initMap(){
-      var location = new google.maps.LatLng(40.382003, 17.367155);
-      var map = new google.maps.Map(document.getElementById("map"),{
-        zoom:18,
-        center:location
-      });
-      <?php 
-        $conn = mysqli_connect("localhost","root","","civicsense") or die("Connessione fallita");
+  <div id="wrapper">
 
-        if(isset($_SESSION['idT'])){
-        $sql = "SELECT * FROM segnalazioni WHERE team = ".$_SESSION['idT'];
-        $result = mysqli_query($conn,$sql);
-        if($result){
-          while($row=mysqli_fetch_assoc($result)){
-            echo "
-            var location = new google.maps.LatLng(".filter_var($row['latitudine'], FILTER_SANITIZE_NUMBER_FLOAT).",".filter_var($row['longitudine'], FILTER_SANITIZE_NUMBER_FLOAT).");
+    <div class="card mb-3">
+
+
+      <!-- MAPPA -->
+
+      <style>
+        #map {
+          height: 500px;
+          width: 100%;
+          margin-left: 0%;
+        }
+
+        * {
+          margin: 0;
+          padding: 0;
+        }
+      </style>
+
+
+      <div id="map"></div>
+
+      <script>
+        function initMap() {
+          var location = new google.maps.LatLng(40.382003, 17.367155);
+          var map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 18,
+            center: location
+          });
+          <?php
+          $conn = mysqli_connect("localhost", "root", "", "civicsense") or die("Connessione fallita");
+
+          if (isset($_SESSION['idT'])) {
+            $query = "SELECT * FROM segnalazioni WHERE team = ?";
+            $stmt = $conn->prepare($query);
+            $stmt->bind_param('s', $_SESSION['idT']);
+            $result_query = $stmt->execute();
+            if ($result_query) {
+              $result = $stmt->get_result();
+              while ($row = mysqli_fetch_assoc($result)) {
+                echo "
+            var location = new google.maps.LatLng(" . filter_var($row['latitudine'], FILTER_SANITIZE_NUMBER_FLOAT) . "," . filter_var($row['longitudine'], FILTER_SANITIZE_NUMBER_FLOAT) . ");
             var marker = new google.maps.Marker({
               map: map,
               position: location
-            }); " ;
+            }); ";
+              }
+              mysqli_close($conn);
+            }
           }
-          mysqli_close($conn);
+          ?>
+          /*var marker = new google.maps.Marker({
+                  map: map,
+                  position: location
+              });
+          var marker1 = new google.maps.Marker({
+            map: map,
+            position: location1
+          });*/
+
         }
-      }
-      ?>
-      /*var marker = new google.maps.Marker({
-              map: map,
-              position: location
-          });
-      var marker1 = new google.maps.Marker({
-        map: map,
-        position: location1
-      });*/
-      
-    }
-    
-    </script>
-    
-      
-  <script async defer 
-  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB7GIu4drL85xcaTdq8hAtRzVWjbKxs3NQ&callback=initMap">
-    </script>
-  
+      </script>
+
+
+      <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB7GIu4drL85xcaTdq8hAtRzVWjbKxs3NQ&callback=initMap">
+      </script>
 
 
 
-			
-			<!-- FINE MAPPA -->
+
+
+      <!-- FINE MAPPA -->
 
 
 
 
 
 
-			<br><br><br>
-			
-			 <div class="card-header">
-              <i class="fas fa-table"></i>
-             Tabella Segnalazioni da risolvere</div>
-			 <br><br>
-            <div class="card-body">
-			 <!-- Tabella -->
-              <div class="table-responsive" style="overflow-x: scroll;" >
-                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0" >
-                  <thead >
-                    <tr>
-                      <th>CODICE SEGNALAZIONE</th>
-                      <th>DATA</th>
-                      <th>ORA</th>
-                      <th>VIA</th>
-                      <th>DESCRIZIONE</th>
-                      <th>FOTO</th>
-					            <th>TIPO</th>
-                      <th>STATO</th>
-					            <th>GRAVITA'</th>
-                    </tr>
-                  </thead>
+      <br><br><br>
 
-<?php include("php/segnalazione.php"); ?>
+      <div class="card-header">
+        <i class="fas fa-table"></i>
+        Tabella Segnalazioni da risolvere
+      </div>
+      <br><br>
+      <div class="card-body">
+        <!-- Tabella -->
+        <div class="table-responsive" style="overflow-x: scroll;">
+          <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+            <thead>
+              <tr>
+                <th>CODICE SEGNALAZIONE</th>
+                <th>DATA</th>
+                <th>ORA</th>
+                <th>VIA</th>
+                <th>DESCRIZIONE</th>
+                <th>FOTO</th>
+                <th>TIPO</th>
+                <th>STATO</th>
+                <th>GRAVITA'</th>
+              </tr>
+            </thead>
+
+            <?php include("php/segnalazione.php"); ?>
 
 
-</table>
+          </table>
 
 
 
@@ -188,31 +191,34 @@
 
 
 
-	<!-- MODIFICA STATO SEGNALAZIONE -->
-	
-<!-- inserimento da form del codice della segnalazione da modificare -->
-<br><br><br>
+          <!-- MODIFICA STATO SEGNALAZIONE -->
 
-<div class="card-header">
-  <i class="fas fa-table"></i>
-Modifica stato di una segnalazione</div>
+          <!-- inserimento da form del codice della segnalazione da modificare -->
+          <br><br><br>
 
-	<form  method="post" action ="modifiche.php" style=" margin-top:5%; margin-left:5%">
-<b>CODICE SEGNALAZIONE DA MODIFICARE: <input type="text" name="id"><br><br></b>
-<b> INSERISCI LO STATO MODIFICATO: </b> <select class="text" name="stato"> 
-   
-    <option value="Risolto">Risolto</option>
-    <option value="In risoluzione">In risoluzione</option>
+          <div class="card-header">
+            <i class="fas fa-table"></i>
+            Modifica stato di una segnalazione
+          </div>
 
-<input type="submit"  class="btn btn-primary btn-block" style="width:15%; margin-top:5%;">
+          <form method="post" action="modifiche.php" style=" margin-top:5%; margin-left:5%">
+            <b>CODICE SEGNALAZIONE DA MODIFICARE: <input type="text" name="id"><br><br></b>
+            <b> INSERISCI LO STATO MODIFICATO: </b> <select class="text" name="stato">
 
-    </form>
+              <option value="Risolto">Risolto</option>
+              <option value="In risoluzione">In risoluzione</option>
 
-<br><br><br>
+              <input type="submit" class="btn btn-primary btn-block" style="width:15%; margin-top:5%;">
+
+          </form>
+
+          <br><br><br>
 
 
-<br><br>
-</div></div></div>
+          <br><br>
+        </div>
+      </div>
+    </div>
 
     <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
@@ -231,17 +237,17 @@ Modifica stato di una segnalazione</div>
     <!-- Demo scripts for this page-->
     <script src="js/demo/datatables-demo.js"></script>
 
-	
-	
-	
 
 
 
-	
-	
-	
-	
-	
-  </body>
+
+
+
+
+
+
+
+
+</body>
 
 </html>
--- Team/php/segnalazione.php
+++ Team/php/segnalazione.php
@@ -11,31 +11,35 @@ if (isset($_SESSION['idT'])) {
 
 
 
-  $quer = mysqli_query($conn, "SELECT * FROM segnalazioni WHERE stato  <> 'Risolto' AND team = " . $_SESSION['idT']);
-
-
-  while ($row = mysqli_fetch_assoc($quer)) {
-    echo "
-    <tr>
-     
-                <td>" . filter_var($row['id'], FILTER_SANITIZE_NUMBER_INT) . " <br></td>
-                
-                <td>" . filter_var($row['datainv'], FILTER_SANITIZE_STRING) . " <br></td> 
-                
-              <td>" . filter_var($row['orainv'], FILTER_SANITIZE_STRING) . "<br></td>
-
-               <td>" . filter_var($row['via'], FILTER_SANITIZE_STRING) . "<br></td>
-
-                <td>" . filter_var($row['descrizione'], FILTER_SANITIZE_STRING) . "<br></td>
-
-                 <td><img width='200px' height='200px' src=" . $upload_path . filter_var($row['foto'], FILTER_SANITIZE_STRING) . "><br></td>
-				  
-				   <td>" . filter_var($row['tipo'], FILTER_SANITIZE_NUMBER_INT) . "<br></td>
-
-                   <td>" . filter_var($row['stato'], FILTER_SANITIZE_STRING) . "<br></td>
-
-                   <td>" . filter_var($row['gravita'], FILTER_SANITIZE_NUMBER_INT) . "<br></td>
-               
-          </tr> ";
+  $query = "SELECT * FROM segnalazioni WHERE stato  <> 'Risolto' AND team = ?";
+  $stmt = $conn->prepare($query);
+  $stmt->bind_param('s', $_SESSION['idT']);
+  $result_query = $stmt->execute();
+  if ($result_query) {
+    $result = $stmt->get_result();
+    while ($row = mysqli_fetch_assoc($result)) {
+      echo "
+      <tr>
+       
+                  <td>" . filter_var($row['id'], FILTER_SANITIZE_NUMBER_INT) . " <br></td>
+                  
+                  <td>" . filter_var($row['datainv'], FILTER_SANITIZE_STRING) . " <br></td> 
+                  
+                <td>" . filter_var($row['orainv'], FILTER_SANITIZE_STRING) . "<br></td>
+  
+                 <td>" . filter_var($row['via'], FILTER_SANITIZE_STRING) . "<br></td>
+  
+                  <td>" . filter_var($row['descrizione'], FILTER_SANITIZE_STRING) . "<br></td>
+  
+                   <td><img width='200px' height='200px' src=" . $upload_path . filter_var($row['foto'], FILTER_SANITIZE_STRING) . "><br></td>
+            
+             <td>" . filter_var($row['tipo'], FILTER_SANITIZE_NUMBER_INT) . "<br></td>
+  
+                     <td>" . filter_var($row['stato'], FILTER_SANITIZE_STRING) . "<br></td>
+  
+                     <td>" . filter_var($row['gravita'], FILTER_SANITIZE_NUMBER_INT) . "<br></td>
+                 
+            </tr> ";
+    }
   }
 }
